9c496d6a62d4d2eac193a78638b28a2f133d5df8
Maniroyal77 <67797945+Maniroyal77@users.noreply.github.com>
2020-07-29 18:47:26 +0530

    Add files via upload

---
+++ size and product.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"enter the values of a,b\n";
+    cin>>a>>b;
+    cout<<"c: "<<a*b;
+    return 0;
+}
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "size of char:"<<size of (char)<<"bytes"<<endl;
+    cout <<"size of int:"<<size of (int)<<"bytes"<<endl;
+    cout <<"size of float:"<<size of (float)<<"bytes"<<endl;
+    cout <<"size of double:"<<size of (double)<<"bytes"<<endl;
+    return 0;
+}
